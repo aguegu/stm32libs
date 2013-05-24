da764902617843a467bd27eb16f02a6c0f859109
Weihong Guan <weihong.guan@gmail.com>
2013-05-24 16:56:43 +0800

    create Gpio class

---
+++ gpio/Gpio.cpp
@@ -0,0 +1,18 @@
+/*
+ * Gpio.cpp
+ *
+ *  Created on: May 24, 2013
+ *      Author: agu
+ */
+
+#include "Gpio.h"
+
+Gpio::Gpio() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Gpio::~Gpio() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ gpio/Gpio.h
@@ -0,0 +1,17 @@
+/*
+ * Gpio.h
+ *
+ *  Created on: May 24, 2013
+ *      Author: agu
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+class Gpio {
+public:
+	Gpio();
+	virtual ~Gpio();
+};
+
+#endif /* GPIO_H_ */
