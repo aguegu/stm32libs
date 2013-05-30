738802291c98244bd07645b7778bbb487386b8a4
Weihong Guan <weihong.guan@gmail.com>
2013-05-30 19:13:29 +0800

    make a i2c class from origin c codes

--- i2c/i2c.cpp
+++ i2c/i2c.cpp
@@ -5,269 +5,105 @@
 
 #include "i2c/i2c.h"
 
-#define Timed(x) Timeout = 0xFFFF; while (x) { if (Timeout-- == 0) goto errReturn;}
-
-/* 
- *  See AN2824 STM32F10xxx I2C optimized examples
- *
- *  This code implements polling based solution
- *
- */
-
-/**
- *  Names of events used in stdperipheral library
- *
- *      I2C_EVENT_MASTER_MODE_SELECT                          : EV5
- *      I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED            : EV6     
- *      I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED               : EV6
- *      I2C_EVENT_MASTER_BYTE_RECEIVED                        : EV7
- *      I2C_EVENT_MASTER_BYTE_TRANSMITTING                    : EV8
- *      I2C_EVENT_MASTER_BYTE_TRANSMITTED                     : EV8_2
- *     
- **/
-
-
-
-/*
- *  Read process is documented in RM008
- *
- *   There are three cases  -- read  1 byte  AN2824 Figure 2 
- *                             read  2 bytes AN2824 Figure 2
- *                             read >2 bytes AN2824 Figure 1
- */  
-
-Status I2C_Read(I2C_TypeDef* I2Cx, uint8_t *buf,uint32_t nbyte, uint8_t SlaveAddress)
-{
-  __IO uint32_t Timeout = 0;
-
-  //    I2Cx->CR2 |= I2C_IT_ERR;  interrupts for errors 
-
-  if (!nbyte)
-    return Success;
-
-
-
-  // Wait for idle I2C interface
-
-  Timed(I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY));
-
-  // Enable Acknowledgement, clear POS flag
-
-  I2C_AcknowledgeConfig(I2Cx, ENABLE);
-  I2C_NACKPositionConfig(I2Cx, I2C_NACKPosition_Current);
-
-  // Intiate Start Sequence (wait for EV5
-
-  I2C_GenerateSTART(I2Cx, ENABLE);
-  Timed(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
-
-  // Send Address
-
-  I2C_Send7bitAddress(I2Cx, SlaveAddress, I2C_Direction_Receiver);
-
-  // EV6
-  
-  Timed(!I2C_GetFlagStatus(I2Cx, I2C_FLAG_ADDR));
-
-  if (nbyte == 1)
-    {
-
-      // Clear Ack bit      
-
-      I2C_AcknowledgeConfig(I2Cx, DISABLE);       
-
-      // EV6_1 -- must be atomic -- Clear ADDR, generate STOP
-
-      __disable_irq();
-      (void) I2Cx->SR2;                           
-      I2C_GenerateSTOP(I2Cx,ENABLE);      
-      __enable_irq();
-
-      // Receive data   EV7
-
-      Timed(!I2C_GetFlagStatus(I2Cx, I2C_FLAG_RXNE));
-      *buf++ = I2C_ReceiveData(I2Cx);
-
-    }
-  else if (nbyte == 2)
-    {
-      // Set POS flag
-
-      I2C_NACKPositionConfig(I2Cx, I2C_NACKPosition_Next);
-
-      // EV6_1 -- must be atomic and in this order
-
-      __disable_irq();
-      (void) I2Cx->SR2;                           // Clear ADDR flag
-      I2C_AcknowledgeConfig(I2Cx, DISABLE);       // Clear Ack bit
-      __enable_irq();
-
-      // EV7_3  -- Wait for BTF, program stop, read data twice
-
-      Timed(!I2C_GetFlagStatus(I2Cx, I2C_FLAG_BTF));
-
-      __disable_irq();
-      I2C_GenerateSTOP(I2Cx,ENABLE);
-      *buf++ = I2Cx->DR;
-      __enable_irq();
-
-      *buf++ = I2Cx->DR;
-
-    }
-  else 
-    {
-      (void) I2Cx->SR2;                           // Clear ADDR flag
-      while (nbyte-- != 3)
-	{
-	  // EV7 -- cannot guarantee 1 transfer completion time, wait for BTF 
-          //        instead of RXNE
-
-	  Timed(!I2C_GetFlagStatus(I2Cx, I2C_FLAG_BTF)); 
-	  *buf++ = I2C_ReceiveData(I2Cx);
-	}
-
-      Timed(!I2C_GetFlagStatus(I2Cx, I2C_FLAG_BTF));  
-
-      // EV7_2 -- Figure 1 has an error, doesn't read N-2 !
-
-      I2C_AcknowledgeConfig(I2Cx, DISABLE);           // clear ack bit
-
-      __disable_irq();
-      *buf++ = I2C_ReceiveData(I2Cx);             // receive byte N-2
-      I2C_GenerateSTOP(I2Cx,ENABLE);                  // program stop
-      __enable_irq();
-
-      *buf++ = I2C_ReceiveData(I2Cx);             // receive byte N-1
-
-      // wait for byte N
-
-      Timed(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED)); 
-      *buf++ = I2C_ReceiveData(I2Cx);
-
-      nbyte = 0;
-
-    }
-
-  // Wait for stop
-
-  Timed(I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF));
-  return Success;
-
- errReturn:
-
-  // Any cleanup here
-  return Error;
-
+I2c::I2c(I2C_TypeDef * i2c, uint32_t rcc_apb1periph_i2cx, uint16_t flat_timeout,
+		uint16_t long_timeout) :
+		_i2c(i2c), _FLAG_TIMEOUT(flat_timeout), _LONG_TIMEOUT(long_timeout) {
+	RCC_APB1PeriphClockCmd(rcc_apb1periph_i2cx, ENABLE);
 }
 
-/*
- * Read buffer of bytes -- AN2824 Figure 3
- */
+void I2c::init(uint16_t mode, uint32_t clock_speed, uint16_t ack,
+		uint16_t acknowledged_address, uint16_t duty_cycle) {
 
-Status I2C_Write(I2C_TypeDef* I2Cx, const uint8_t* buf,  uint32_t nbyte, uint8_t SlaveAddress)
-{
-    __IO uint32_t Timeout = 0;
+	I2C_DeInit(_i2c);
 
-    /* Enable Error IT (used in all modes: DMA, Polling and Interrupts */
-    //    I2Cx->CR2 |= I2C_IT_ERR;
+	I2C_InitTypeDef I2C_InitStructure;
+	I2C_StructInit(&I2C_InitStructure);
+	I2C_InitStructure.I2C_Mode = mode;
+	I2C_InitStructure.I2C_DutyCycle = duty_cycle;
+	I2C_InitStructure.I2C_OwnAddress1 = acknowledged_address;
+	I2C_InitStructure.I2C_Ack = ack;
+	I2C_InitStructure.I2C_ClockSpeed = clock_speed;
+	I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
 
-    if (nbyte)
-      {
-	Timed(I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY));
-
-	// Intiate Start Sequence
-
-	I2C_GenerateSTART(I2Cx, ENABLE);
-	Timed(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
-
-	// Send Address  EV5
-
-	I2C_Send7bitAddress(I2Cx, SlaveAddress, I2C_Direction_Transmitter);
-	Timed(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-
-	// EV6
-
-	// Write first byte EV8_1
-
-	I2C_SendData(I2Cx, *buf++);
-
-	while (--nbyte) {
-
-	  // wait on BTF
-
-	  Timed(!I2C_GetFlagStatus(I2Cx, I2C_FLAG_BTF));  
-	  I2C_SendData(I2Cx, *buf++);
-	}
-
-	Timed(!I2C_GetFlagStatus(I2Cx, I2C_FLAG_BTF));  
-	I2C_GenerateSTOP(I2Cx, ENABLE);
-	Timed(I2C_GetFlagStatus(I2C1, I2C_FLAG_STOPF));
-      }
-    return Success;
- errReturn:
-    return Error;
+	I2C_Init(_i2c, &I2C_InitStructure);
+	I2C_Cmd(_i2c, ENABLE);
 }
 
+uint8_t I2c::write(uint8_t slave_address, const uint8_t* buf, uint32_t length) {
+	vu32 t = 0;
 
-void I2C_LowLevel_Init(I2C_TypeDef* I2Cx, int ClockSpeed, int OwnAddress)
-{
+	if (length) {
+		for (t = _LONG_TIMEOUT; I2C_GetFlagStatus(_i2c, I2C_FLAG_BUSY ) && --t;)
+			;
+		if (!t) return 0;
 
-    GPIO_InitTypeDef  GPIO_InitStructure;
-    I2C_InitTypeDef  I2C_InitStructure;
+		I2C_GenerateSTART(_i2c, ENABLE);
+		for (t = _FLAG_TIMEOUT;
+				!I2C_CheckEvent(_i2c, I2C_EVENT_MASTER_MODE_SELECT ) && --t;)
+			;
+		if (!t) return 1;
 
-    // Enable GPIOB clocks
+		I2C_Send7bitAddress(_i2c, slave_address, I2C_Direction_Transmitter );
+		for (t = _FLAG_TIMEOUT;
+				!I2C_CheckEvent(_i2c,
+						I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) && --t;)
+			;
+		if (!t) return 2;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+		do {
+			I2C_SendData(_i2c, *buf++);
+			for (t = _FLAG_TIMEOUT; !I2C_GetFlagStatus(_i2c, I2C_FLAG_BTF ) && --t;)
+				;
+			if (!t) return 3;
+		} while (--length);
 
-    // Configure I2C clock and GPIO 
+		I2C_GenerateSTOP(_i2c, ENABLE);
+		for (t = _FLAG_TIMEOUT; I2C_GetFlagStatus(I2C1, I2C_FLAG_STOPF ) && --t;)
+			;
+		if (!t) return 4;
+	}
+	return 5;
+}
 
-    GPIO_StructInit(&GPIO_InitStructure);
+uint8_t I2c::read(uint8_t slave_address, uint8_t* data, uint32_t length) {
+	vu32 t = 0;
+	if (length) {
 
+		for (t = _LONG_TIMEOUT; I2C_GetFlagStatus(_i2c, I2C_FLAG_BUSY ) && --t;)
+			;
+		if (!t) return 0;
 
-    if (I2Cx == I2C1){
+		I2C_AcknowledgeConfig(_i2c, ENABLE);
+		I2C_NACKPositionConfig(_i2c, I2C_NACKPosition_Current );
+		I2C_GenerateSTART(_i2c, ENABLE);
 
-        /* I2C1 clock enable */
+		for (t = _FLAG_TIMEOUT;
+				!I2C_CheckEvent(_i2c, I2C_EVENT_MASTER_MODE_SELECT ) && --t;)
+			;
+		if (!t) return 1;
 
-        RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+		I2C_Send7bitAddress(_i2c, slave_address, I2C_Direction_Receiver );
+		for (t = _FLAG_TIMEOUT; !I2C_GetFlagStatus(_i2c, I2C_FLAG_ADDR ) && --t;)
+			;
+		if (!t) return 2;
 
-        /* I2C1 SDA and SCL configuration */
+		I2C_AcknowledgeConfig(_i2c, DISABLE);
 
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
-        GPIO_Init(GPIOB, &GPIO_InitStructure);
+		__disable_irq();
+		(void) _i2c->SR2;
+		I2C_GenerateSTOP(_i2c, ENABLE);
+		__enable_irq();
 
-        /* I2C1 Reset */
-        I2C_DeInit(I2C1);
-      }
-      else {
+		for (t = _FLAG_TIMEOUT; !I2C_GetFlagStatus(_i2c, I2C_FLAG_RXNE ) && --t;)
+			;
+		if (!t) return 3;
 
-        /* I2C2 clock enable */
+		*data = I2C_ReceiveData(_i2c);
+	}
 
-        RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
+	for (t = _FLAG_TIMEOUT; I2C_GetFlagStatus(_i2c, I2C_FLAG_STOPF ) && --t;)
+		;
+	if (!t) return 4;
 
-        /* I2C1 SDA and SCL configuration */
-
-        GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
-        GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-        GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
-        GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-        /* I2C2  Reset */
-
-        I2C_DeInit(I2C2);
-      }
-
-    /* Configure I2Cx                */
-
-    I2C_StructInit(&I2C_InitStructure);
-    I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
-    I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
-    I2C_InitStructure.I2C_OwnAddress1 = OwnAddress;
-    I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
-    I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-    I2C_InitStructure.I2C_ClockSpeed = ClockSpeed;
-
-    I2C_Init(I2Cx, &I2C_InitStructure);
-    I2C_Cmd(I2Cx, ENABLE); 
+	return 5;
 }
--- i2c/i2c.h
+++ i2c/i2c.h
@@ -3,10 +3,31 @@
 
 #include <stm32f10x_i2c.h>
 
-typedef enum {Error = 0, Success = !Error } Status;
+typedef enum {
+	Error = 0, Success = !Error
+} Status;
 
-Status I2C_Read(I2C_TypeDef* I2Cx, uint8_t* buf, uint32_t nbuf, uint8_t SlaveAddress);
-Status I2C_Write(I2C_TypeDef* I2Cx, const uint8_t* buf, uint32_t nbuf,  uint8_t SlaveAddress);
-void I2C_LowLevel_Init(I2C_TypeDef* I2Cx, int ClockSpeed, int OwnAddress);
+Status I2C_Read(I2C_TypeDef* I2Cx, uint8_t* buf, uint32_t nbuf,
+		uint8_t SlaveAddress);
+Status I2C_Write(I2C_TypeDef* I2Cx, const uint8_t* buf, uint32_t nbuf,
+		uint8_t SlaveAddress);
+void I2C_LowLevel_Init(int ClockSpeed, int OwnAddress);
+
+class I2c {
+public:
+	I2c(I2C_TypeDef * i2c, uint32_t rcc_apb1periph_i2cx, uint16_t flat_timeout =
+			0x1000, uint16_t long_timeout = 0xf000);
+	~I2c();
+	void init(uint16_t mode = I2C_Mode_I2C, uint32_t clock_speed = 100000,
+			uint16_t ack = I2C_Ack_Enable, uint16_t acknowledged_address = 0,
+			uint16_t duty_cycle = I2C_DutyCycle_2 );
+
+	uint8_t write(uint8_t slave_address, const uint8_t* buf, uint32_t length);
+	uint8_t read(uint8_t slave_address, uint8_t* buf, uint32_t length);
+private:
+	I2C_TypeDef * const _i2c;
+	const uint16_t _FLAG_TIMEOUT;
+	const uint16_t _LONG_TIMEOUT;
+};
 
 #endif
