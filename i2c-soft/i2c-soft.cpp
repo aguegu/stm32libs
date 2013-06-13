6cbe6c768dd4caae1387946d4ed0386ef82efc3c
Weihong.Guan <weihong.guan@gmail.com>
2013-06-13 22:13:57 +0800

    simplify soft i2c

--- i2c-soft/i2c-soft.cpp
+++ i2c-soft/i2c-soft.cpp
@@ -30,11 +30,10 @@ void I2cSoft::start() {
 	_sda.init(GPIO_Mode_Out_OD);
 	_sda.set(Bit_SET);
 
-	_scl.set(Bit_SET);
-	delayMicroseconds(4);
+//	_scl.set(Bit_SET);
+//	delayMicroseconds(4);
 	_sda.set(Bit_RESET);
-	delayMicroseconds(4);
-
+//	delayMicroseconds(4);
 	_scl.set(Bit_RESET);
 }
 
@@ -44,9 +43,9 @@ void I2cSoft::stop() {
 
 	_sda.set(Bit_RESET);
 	_scl.set(Bit_SET);
-	delayMicroseconds(8);
+//	delayMicroseconds(8);
 	_sda.set(Bit_SET);
-	delayMicroseconds(8);
+//	delayMicroseconds(8);
 
 }
 
@@ -54,15 +53,15 @@ void I2cSoft::transmit(uint8_t data) {
 
 	_sda.init(GPIO_Mode_Out_OD);
 
-	_scl.set(Bit_RESET);
+//	_scl.set(Bit_RESET);
 
 	for (uint8_t i = 0; i < 8; i++) {
 		_sda.set(data & 0x80 ? Bit_SET : Bit_RESET);
-		delayMicroseconds(4);
+//		delayMicroseconds(4);
 		_scl.set(Bit_SET);
-		delayMicroseconds(4);
+//		delayMicroseconds(4);
 		_scl.set(Bit_RESET);
-		delayMicroseconds(4);
+//		delayMicroseconds(4);
 		data <<= 1;
 	}
 }
@@ -74,13 +73,15 @@ uint8_t I2cSoft::receive() {
 	_sda.init(GPIO_Mode_IN_FLOATING);
 
 	for (u8 i = 0; i < 8; i++) {
-		_scl.set(Bit_RESET);
-		delayMicroseconds(2);
+
+//		delayMicroseconds(2);
 		_scl.set(Bit_SET);
-		delayMicroseconds(2);
+//		delayMicroseconds(2);
 		data <<= 1;
 		data |= _sda.getInput() ? 0x01 : 0x00;
-		delayMicroseconds(1);
+//		delayMicroseconds(1);
+
+		_scl.set(Bit_RESET);
 	}
 
 	return data;
@@ -90,12 +91,11 @@ u16 I2cSoft::waitAck() {
 
 	_sda.init(GPIO_Mode_IN_FLOATING);
 
-	vu16 t;
-
-	delayMicroseconds(4);
+//	delayMicroseconds(4);
 	_scl.set(Bit_SET);
-	delayMicroseconds(4);
+//	delayMicroseconds(4);
 
+	vu16 t;
 	within(_FLAG_TIMEOUT, _sda.getInput() == Bit_SET);
 
 	_scl.set(Bit_RESET);
@@ -104,10 +104,7 @@ u16 I2cSoft::waitAck() {
 
 uint8_t I2cSoft::write(uint8_t address, uint8_t *data, uint8_t length,
 		uint8_t send_stop) {
-
 	vu16 t;
-
-	if (!length) return 0;
 
 	within(_LONG_TIMEOUT, _sda.getInput() == Bit_RESET);
 	if (!t) return 1;
@@ -151,8 +148,8 @@ void I2cSoft::sendAck(uint8_t ack) {
 	_sda.init(GPIO_Mode_Out_OD);
 	_sda.set(ack ? Bit_RESET : Bit_SET);
 
-	delayMicroseconds(2);
+//	delayMicroseconds(2);
 	_scl.set(Bit_SET);
-	delayMicroseconds(2);
+//	delayMicroseconds(2);
 	_scl.set(Bit_RESET);
 }
