9a5965acd3319600e2817457aee31a1437f169ba
Weihong Guan <weihong.guan@gmail.com>
2013-07-04 14:51:58 +0800

    TimIc class

--- tim/tim.cpp
+++ tim/tim.cpp
@@ -54,16 +54,17 @@ void TimOc::init(uint16_t oc_mode, uint16_t output_state,
 		uint16_t oc_npolarity, uint16_t oc_idle_state,
 		uint16_t oc_nidle_state) {
 
-	_tim_oc_init_type.TIM_OCMode = oc_mode;
-	_tim_oc_init_type.TIM_OutputState = output_state;
-	_tim_oc_init_type.TIM_OutputNState = output_nstate;
-	_tim_oc_init_type.TIM_Pulse = pulse;
-	_tim_oc_init_type.TIM_OCPolarity = oc_polarity;
-	_tim_oc_init_type.TIM_OCNPolarity = oc_npolarity;
-	_tim_oc_init_type.TIM_OCIdleState = oc_idle_state;
-	_tim_oc_init_type.TIM_OCNIdleState = oc_nidle_state;
+	TIM_OCInitTypeDef tim_oc_init_type;
+	tim_oc_init_type.TIM_OCMode = oc_mode;
+	tim_oc_init_type.TIM_OutputState = output_state;
+	tim_oc_init_type.TIM_OutputNState = output_nstate;
+	tim_oc_init_type.TIM_Pulse = pulse;
+	tim_oc_init_type.TIM_OCPolarity = oc_polarity;
+	tim_oc_init_type.TIM_OCNPolarity = oc_npolarity;
+	tim_oc_init_type.TIM_OCIdleState = oc_idle_state;
+	tim_oc_init_type.TIM_OCNIdleState = oc_nidle_state;
 
-	(*_tim_ocx_init)(_tim, &_tim_oc_init_type);
+	(*_tim_ocx_init)(_tim, &tim_oc_init_type);
 }
 
 void TimOc::setCompare(uint16_t pulse) {
@@ -72,7 +73,8 @@ void TimOc::setCompare(uint16_t pulse) {
 
 //////////////////////////////////////////////////////////////
 
-TimIc::TimIc() {
+TimIc::TimIc(TIM_TypeDef * tim) :
+		_tim(tim) {
 
 }
 
@@ -82,9 +84,11 @@ TimIc::~TimIc() {
 
 void TimIc::init(uint16_t channel, uint16_t ic_polarity, uint16_t ic_selection,
 		uint16_t ic_prescaler, uint16_t ic_filter) {
-	_tim_ic_init_type.TIM_Channel = channel;
-	_tim_ic_init_type.TIM_ICPolarity = ic_polarity;
-	_tim_ic_init_type.TIM_ICSelection = ic_selection;
-	_tim_ic_init_type.TIM_ICPrescaler = ic_prescaler;
-	_tim_ic_init_type.TIM_ICFilter = ic_filter;
+	TIM_ICInitTypeDef tim_ic_init_type;
+	tim_ic_init_type.TIM_Channel = channel;
+	tim_ic_init_type.TIM_ICPolarity = ic_polarity;
+	tim_ic_init_type.TIM_ICSelection = ic_selection;
+	tim_ic_init_type.TIM_ICPrescaler = ic_prescaler;
+	tim_ic_init_type.TIM_ICFilter = ic_filter;
+	TIM_ICInit(_tim, &tim_ic_init_type);
 }
--- tim/tim.h
+++ tim/tim.h
@@ -12,13 +12,15 @@
 
 class Tim {
 public:
-	Tim(TIM_TypeDef * tim, uint32_t rcc_apbx_periph,
-			void (*p)(uint32_t, FunctionalState));
+	Tim(TIM_TypeDef * tim,
+		uint32_t rcc_apbx_periph,
+		void (*p)(uint32_t, FunctionalState));
 	~Tim();
-	void init(uint32_t real_clock = SystemCoreClock, uint16_t real_period = 0,
-			uint16_t counter_mode = TIM_CounterMode_Up,
-			uint16_t clock_division = TIM_CKD_DIV1, uint8_t repition_counter =
-					0x0000);
+	void init(uint32_t real_clock = SystemCoreClock,
+		uint16_t real_period = 0,
+		uint16_t counter_mode = TIM_CounterMode_Up,
+		uint16_t clock_division = TIM_CKD_DIV1,
+		uint8_t repition_counter = 0x0000);
 private:
 	TIM_TypeDef * const _tim;
 };
@@ -26,40 +28,40 @@ private:
 class TimOc {
 public:
 	TimOc(TIM_TypeDef * tim,
-			void (*tim_ocx_init)(TIM_TypeDef* TIMx,
-					TIM_OCInitTypeDef* TIM_OCInitStruct),
-			void (*tim_set_comparex)(TIM_TypeDef* TIMx, uint16_t compare));
+		void (*tim_ocx_init)(TIM_TypeDef* TIMx,
+			TIM_OCInitTypeDef* TIM_OCInitStruct),
+		void (*tim_set_comparex)(TIM_TypeDef* TIMx, uint16_t compare));
 	~TimOc();
 
-	void init(uint16_t oc_mode = TIM_OCMode_Timing, uint16_t output_state =
-	TIM_OutputState_Disable, uint16_t output_nstate =
-	TIM_OutputNState_Disable, uint16_t pulse = 0x0000, uint16_t oc_polarity =
-	TIM_OCPolarity_High, uint16_t oc_npolarity =
-	TIM_OCPolarity_High, uint16_t oc_idle_state =
-	TIM_OCIdleState_Reset, uint16_t oc_nidle_state =
-	TIM_OCNIdleState_Reset);
+	void init(uint16_t oc_mode = TIM_OCMode_Timing,
+		uint16_t output_state = TIM_OutputState_Disable,
+		uint16_t output_nstate = TIM_OutputNState_Disable,
+		uint16_t pulse = 0x0000,
+		uint16_t oc_polarity = TIM_OCPolarity_High,
+		uint16_t oc_npolarity = TIM_OCPolarity_High,
+		uint16_t oc_idle_state = TIM_OCIdleState_Reset,
+		uint16_t oc_nidle_state = TIM_OCNIdleState_Reset);
 
 	void setCompare(uint16_t pulse);
 
 private:
-	TIM_OCInitTypeDef _tim_oc_init_type;
 	TIM_TypeDef * const _tim;
 	void (*_tim_ocx_init)(TIM_TypeDef* TIMx,
-			TIM_OCInitTypeDef* TIM_OCInitStruct);
+		TIM_OCInitTypeDef* TIM_OCInitStruct);
 	void (*_tim_set_comparex)(TIM_TypeDef* TIMx, uint16_t compare);
 };
 
 class TimIc {
 public:
-	TimIc();
+	TimIc(TIM_TypeDef * tim);
 	~TimIc();
-	void init(uint16_t channel = TIM_Channel_1, uint16_t ic_polarity =
-	TIM_ICPolarity_Rising, uint16_t ic_selection =
-	TIM_ICSelection_DirectTI, uint16_t ic_prescaler = TIM_ICPSC_DIV1,
-			uint16_t ic_filter = 0x00);
-	void apply();
+	void init(uint16_t channel = TIM_Channel_1,
+		uint16_t ic_polarity = TIM_ICPolarity_Rising,
+		uint16_t ic_selection = TIM_ICSelection_DirectTI,
+		uint16_t ic_prescaler = TIM_ICPSC_DIV1,
+		uint16_t ic_filter = 0x00);
 private:
-	TIM_ICInitTypeDef _tim_ic_init_type;
+	TIM_TypeDef * const _tim;
 };
 
 #endif /* TIM_H_ */
