79ef5289372643208ba01e2ab355310ada91dd11
Weihong Guan <weihong.guan@gmail.com>
2013-07-05 11:14:14 +0800

    adc class

--- adc/adc.cpp
+++ adc/adc.cpp
@@ -7,12 +7,52 @@
 
 #include "adc.h"
 
-Adc::Adc() {
-	// TODO Auto-generated constructor stub
-
+Adc::Adc(ADC_TypeDef *adc, uint32_t rcc_apb2periph_adcx) :
+		_adc(adc) {
+	RCC_APB2PeriphClockCmd(rcc_apb2periph_adcx, ENABLE);
 }
 
 Adc::~Adc() {
-	// TODO Auto-generated destructor stub
 }
 
+void Adc::init(uint32_t mode,
+	FunctionalState scan_conv_mode,
+	FunctionalState continuous_conv_mode,
+	uint32_t external_trig_conv,
+	uint32_t data_align,
+	uint8_t nbr_of_channel) {
+
+	ADC_InitTypeDef adc_init_type;
+
+	adc_init_type.ADC_Mode = mode;
+	adc_init_type.ADC_ScanConvMode = scan_conv_mode;
+	adc_init_type.ADC_ContinuousConvMode = continuous_conv_mode;
+	adc_init_type.ADC_ExternalTrigConv = external_trig_conv;
+	adc_init_type.ADC_DataAlign = data_align;
+	adc_init_type.ADC_NbrOfChannel = nbr_of_channel;
+
+	ADC_Init(_adc, &adc_init_type);
+	ADC_Cmd(_adc, ENABLE);
+}
+
+void Adc::configChannel(uint8_t channel, uint8_t rank, uint8_t sample_time) {
+	ADC_RegularChannelConfig(_adc, channel, rank, sample_time);
+}
+
+void Adc::calibrate() {
+	while (ADC_GetResetCalibrationStatus(_adc))
+		;
+	ADC_StartCalibration(_adc);
+	while (ADC_GetCalibrationStatus(_adc))
+		;
+}
+
+void Adc::startSoftwareConvert(FunctionalState state) {
+	ADC_SoftwareStartConvCmd(ADC1, state);
+}
+
+uint16_t Adc::getValue() {
+	while (ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET)
+		;
+	return ADC_GetConversionValue(ADC1);
+}
--- adc/adc.h
+++ adc/adc.h
@@ -8,10 +8,26 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+#include "stm32f10x_adc.h"
+
 class Adc {
 public:
-	Adc();
+	Adc(ADC_TypeDef *adc, uint32_t rcc_apb2periph_adcx);
 	~Adc();
+
+	void init(uint32_t mode = ADC_Mode_Independent,
+		FunctionalState scan_conv_mode = DISABLE,
+		FunctionalState continuous_conv_mode = DISABLE,
+		uint32_t external_trig_conv = ADC_ExternalTrigConv_T1_CC1,
+		uint32_t data_align = ADC_DataAlign_Right,
+		uint8_t nbr_of_channel = 1);
+
+	void configChannel(uint8_t channel, uint8_t rank, uint8_t sample_time = ADC_SampleTime_239Cycles5);
+	void calibrate();
+	void startSoftwareConvert(FunctionalState state = ENABLE);
+	uint16_t getValue();
+private:
+	ADC_TypeDef* const _adc;
 };
 
 #endif /* ADC_H_ */
