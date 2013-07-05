ae3783b3282ea9421b440d173be9f142a069666d
Weihong Guan <weihong.guan@gmail.com>
2013-07-05 09:10:53 +0800

    init class Adc

---
+++ adc/adc.cpp
@@ -0,0 +1,18 @@
+/*
+ * adc.cpp
+ *
+ *  Created on: Jul 5, 2013
+ *      Author: agu
+ */
+
+#include "adc.h"
+
+Adc::Adc() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Adc::~Adc() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ adc/adc.h
@@ -0,0 +1,17 @@
+/*
+ * adc.h
+ *
+ *  Created on: Jul 5, 2013
+ *      Author: agu
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+class Adc {
+public:
+	Adc();
+	~Adc();
+};
+
+#endif /* ADC_H_ */
